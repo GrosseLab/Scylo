09a469e6e7916f950c0fa55671e28e93e6d1dc34
Fabian Bull <bull.fabian@googlemail.com>
2014-05-13 17:11:48 +0200

    Deleted all linear algebra code and added EJML.

--- src/main/scala/org/scylo/util/EigenSystem.scala
+++
@@ -1,6 +0,0 @@
-package org.scylo.util
-
-case class EigenSystem private[util] ( 
-  val eigenVec: Matrix,
-  val invEigenVec: Matrix,
-  val eigenVal: Array[Double] )
--- src/main/scala/org/scylo/util/MatrixExp.scala
+++
@@ -1,3 +0,0 @@
-package org.scylo.util
-
-case class MatrixExp()
